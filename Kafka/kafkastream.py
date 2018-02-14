b3cb2a7cd381fe326a273dc14b405fa037f13ff4
alejandroferrandis <5419619+alejandroferrandis@users.noreply.github.com>
2018-02-14 18:20:47 +0100

    Update kafkastream.py

--- Kafka/kafkastream.py
+++ Kafka/kafkastream.py
@@ -1,25 +1,27 @@
-import tweepy
-
-from kafka import *
+from tweepy.streaming import StreamListener
+from tweepy import OAuthHandler
+from tweepy import Stream
 from comun.util import *
-from comun.listeners2 import *
+from comun.listeners import *
+from kafka import SimpleProducer, KafkaClient
+import json
+import codecs
 
-if __name__ == '__main__':
 
-    print("===== My Application =====")
+class StdOutListener(StreamListener):
+    def on_data(self, data):
+    	decoded = json.loads(data)
+        print(decoded['text'])
+        producer.send_messages("<>", data.encode('utf-8'))
+        return True
+    def on_error(self, status):
+        print (status)
 
-    auth = get_auth()  
-    api = tweepy.API(auth) 
-
-    producer = KafkaProducer(bootstrap_servers='localhost:9092')
-    topic_name = 'trump'
-
-    myStreamListener = MyStreamListener2()
-    myStream = tweepy.Stream(auth=api.auth, listener=myStreamListener)
-    
-    # Establish your filters
-    
-    SPAIN_GEOBOX = [-9.38,36.05,3.35,43.75]
-    myStream.filter(languages=["es"], locations=SPAIN_GEOBOX)
-
-    print("c'est fini!")
+kafka = KafkaClient("localhost:9092")
+producer = SimpleProducer(kafka)
+l = StdOutListener()
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_token_secret)
+stream = Stream(auth, l)
+SPAIN_GEOBOX = [-9.38,36.05,3.35,43.75]
+stream.filter(languages=["es"], locations=SPAIN_GEOBOX)
